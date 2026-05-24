d42f5c36e9c715f3c94ac6053ca06fd82cf8cd67
Tvle <jonghyeon88@gmail.com>
2026-05-24 22:51:28 +0900

    푸터 수정작업 1

--- src/App.jsx
+++ src/App.jsx
@@ -36,7 +36,7 @@ function App() {
       <PostForm onAddPost={handleAddPost} />
       <PostList posts={posts} onDelete={handleDelete} onLike={handleLike} />
       <div className="footer">
-        <p>확인</p>
+        <p>확인 1</p>
         <p>&copy; 2023 My Blog. All rights reserved.</p>
       </div>
     </>
